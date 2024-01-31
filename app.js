833eda6b27f3ee24e5811751279d97a5939d29ed
lori <wgsxsm@gmail.com>
2024-01-31 10:46:54 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const WebSocket = require('ws');
 const logcb = (...args) => console.log.bind(this, ...args);
 const errcb = (...args) => console.error.bind(this, ...args);
 
-const uuid = (process.env.UUID || '37a0bd7c-8b9f-4693-8916-bd1e2da0a817').replace(/-/g, '');
-const port = process.env.PORT || 3000;
+const uuid = (process.env.UUID || 'aad34b29-4cc9-4332-a1d4-7d5a550c8f16').replace(/-/g, '');
+const port = process.env.PORT || 15149;
 
 const wss = new WebSocket.Server({ port }, logcb('listen:', port));
 
